d6cded4c646079a39f957af27bd8b2b875378241
randazzle <rojanchampoo12@gmail.com>
2021-02-15 18:40:00 +0545

    Added login and Signup

--- backend/TourismAid/TourismAid/urls.py
+++ backend/TourismAid/TourismAid/urls.py
@@ -8,6 +8,9 @@ from articles import views as article_views
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
     path('about/', views.about),
     path('accounts/', include('accounts.urls')),
